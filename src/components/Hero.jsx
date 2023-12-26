1f7d23dd885af908a9a8672230e857794b1f3a7f
Akshaybalapure7 <balapureap10@gmail.com>
2023-12-26 18:38:32 +0530

    Changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,26 +7,28 @@ import main from  '../assets/main.jpg';
 const Hero = () => {
   return (
      <section className="relative w-full h-screen mx-auto">
-          <div className={`${styles.paddingX} absolute inset-0 top-[120px]
-                            max-w-7xl mx-auto flex flex-row items-start gap-5
+          <div className={`${styles.paddingX} absolute inset-0 top-[120px] 
+                            max-w-7xl mx-auto flex flex-row flex-wrap items-start gap-5 
                          `}>
-               <div className="flex flex-col justify-center items-center mt-5">
-                      <div className=" w-5 h-5 rounded-full bg-[#915eff] "/>
-                      <div className="w-0.5 sm:h-80 h-40   violet-gradient "/>
-               </div>
-               <div>
-                    <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm
-                      <span className="text-[#915eff]"> Akshay</span>
-                    </h1>
-                    <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-                          I developed designtools , user <br className="sm:block hidden"/> interfaces and web applications
-                    </p>
-                    
+                <div className="flex gap-8">          
+                  <div className="flex flex-col justify-center items-center mt-5">
+                          <div className=" w-5 h-5 rounded-full bg-[#915eff] "/>
+                          <div className="w-0.5 sm:h-80 h-40   violet-gradient "/>
+                  </div>
+                  <div>
+                        <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm
+                          <span className="text-[#915eff]"> Akshay</span>
+                        </h1>
+                        <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+                              I developed designtools , user <br className="sm:block hidden"/> interfaces and web applications
+                        </p>
+                        
 
+                  </div>
               </div>
-              <div className="flex place-content-center  xs:w-[320px] w-[24rem] float-right mt-5" style={{clipPath:`polygon(100% 0, 85% 49%, 100% 100%, 24% 100%, 0% 50%, 23% 0)`}}>
-                            <img src={main} className=""/>
-                    </div>
+              <div className="flex place-content-center  sm:w-[220px] w-[24rem] float-right " style={{clipPath:`polygon(100% 0, 85% 49%, 100% 100%, 24% 100%, 0% 50%, 23% 0)` , marginLeft:"auto"}}>
+                  <img src={main} className=""/>
+              </div>
           </div>
           {/* <div className="flex place-content-center" style={{height:"100vh"}}>
               <ComputersCanvas/>
